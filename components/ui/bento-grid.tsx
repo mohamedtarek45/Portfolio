c0d998539313efb5ab2654506c71f513f867186d
mohamedtarek45 <moka76615@gmail.com>
2025-02-14 20:07:17 +0200

    test

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./background-gradient-animation";
 
 import CopyEmailAddress from "../CopyEmailAddress";
+import { GlobeDemo } from "./gridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -97,7 +98,7 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-          {/* {id === 2 && <GlobeDemo />} */}
+          {id === 2 && <GlobeDemo />}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:right-[30px] lg:opacity-50">
               <div className="flex flex-col gap-3 ">
